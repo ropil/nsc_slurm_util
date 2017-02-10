331d6815e120167621c9b637ddbfe762f0afbcec
Robert Pilstål <robert.pilstal@liu.se>
2017-02-10 12:40:29 +0100

    Launches jobs into devel partition
    Same as launch jobs, but for development partition

---
+++ nsc_launch_devel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo "USAGE: $0 JOB[ JOB[ ...]]"
+	exit
+fi
+
+me=`whoami`;
+all_launched=`squeue -u $me`;
+
+#for each job specified
+for i in $*; do
+	jobname=`echo $i|awk -F . '{print $1}'`;
+	launched=`grep $jobname <<< $all_launched`;
+	fin=`grep "#FINISH" $i|awk '{print $2}'`;
+	#if not finished
+	if [ ! -e $fin ]; then
+		#And not launched
+		if [ -z "$launched" ]; then
+			#Launch
+			sbatch --reservation=devel -J $jobname $i;
+		fi;
+	fi;
+done;
