52c2b25499175cfe459a0ef14ee8315b98fa6985
Robert Pilstål <robert.pilstal@liu.se>
2017-02-10 12:32:07 +0100

    Assigns jobs
    To be used in a for loop; creates a job script wrapper around a
    duty-script that you specify.
    
    Handled by job handlers generated by nsc_launch_create_handler.sh
    or ran standalone.

---
+++ nsc_jobs_ass.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -e
+
+me=`whoami`;
+
+if [ -z $6 ]; then
+	echo "USAGE: $0 <jobname> <hours> <startnum> <num> <target,target,target> [command and parameters]"
+	echo ""
+	echo " If \$NUMCORES is set (currently \"$NUMCORES\") the scripts will"
+        echo "   be sent to cores (-n <num>) and not to nodes (-N)."
+	echo ""
+	echo "EXAMPLES:"
+	echo "-- pconsc2 prepred --"
+	echo 'num=1; for i in ????.fasta; do nsc_jobs_ass.sh pc2 24 $num 1 $i seqcov_pconsc2_prepred.sh 16 /nobackup/global/$me/db/uniprot20_2012_10/uniprot20_2012_10_klust20_dc_2012_12_10 /nobackup/global/$me/db/uniprot_trembl.fasta $i; let num=num+1; done'
+	echo "-- plmdca nofrob --"
+	echo 'num=1; for i in *.trimmed; do nsc_jobs_ass.sh plm 72 $num 1 $i seqcov_plmdca_nofrob.sh 8 $i; let num=num+1; done'
+	echo "-- rosetta cm --"
+	echo 'num=1; for i in `ls -d ./*/`; do name=`echo $i|awk -F / '\''{print $2}'\''`; for j in `ls $name/${name}*.filtered`; do fname=`echo $j|awk -F / '\''{print $NF}'\''|awk -F . '\''{print $1}'\''`; nsc_jobs_ass.sh hom 5 $num 1 $j homodock_run_rosettacm.sh 4 ${fname}*.fasta ${fname}*.filtered $name; let num=num+1; done; done'
+	exit
+fi
+
+name=$1
+hours=$2
+snum=$3
+let num=$snum+$4-1
+targets=$5
+cmd=${@:6}
+cores=${NUMCORES}
+account=`nsc_account_get.sh`
+
+for i in `seq $snum $num`; do
+	jobname=${name}_`printf "%03d" $i`
+	jobfile=$jobname.job
+	finish=$jobname.fin
+	lock=$jobname.lck
+	echo "#!/bin/bash" > $jobfile
+        if [ -z ${cores} ]; then
+		echo "#SBATCH -N 1" >> $jobfile
+        else
+		echo "#SBATCH -n ${cores}" >> $jobfile
+	fi
+	echo "#SBATCH -t $hours:00:00" >> $jobfile
+	echo "#SBATCH -A $account" >> $jobfile
+	for target in `echo $targets|awk -F , '{for (i=1;i<=NF;i++) printf "%s\n", $i;}'`; do
+		echo "#TARGET $target" >> $jobfile
+	done
+	echo "#FINISH $finish" >> $jobfile
+	echo "#LOCK $lock" >> $jobfile
+	echo "set -e" >> $jobfile
+	echo "echo Running: $jobfile" >> $jobfile
+	echo "echo START: \`date\`" >> $jobfile
+	echo "$cmd" >> $jobfile
+	echo "if [ \$? -eq 0 ]; then" >> $jobfile
+       	echo "	touch $finish" >> $jobfile
+	echo "fi" >> $jobfile
+	echo "echo DONE: \`date\`" >> $jobfile
+	
+	echo "Created job: $jobfile"
+done
